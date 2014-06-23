6a984164207c9ee8173db50778d0f7f131850b4d
Ururu <besteto@gmail.com>
2014-06-23 16:43:14 +0400

    new water shader + upd engine

---
+++ matrix.py
@@ -0,0 +1,40 @@
+from math import *
+from numpy import *
+import ctypes
+
+class mat4:
+    __M = None
+
+    def __init__(self):
+        self.__M = identity(4, dtype=float32)
+
+    @property
+    def ptr(self):
+        return self.__M.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+
+    def translate(self, x, y, z):
+        self.__M = self.__M * matrix([[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]],dtype=float32)
+
+    def scale(self, x, y, z):
+        self.__M = self.__M * matrix([[x, 0, 0, 0], [0, y, 0, 0], [0, 0, z, 0], [0, 0, 0, 1]],dtype=float32)
+
+    def rotate_x(self, a):
+        s = sin(radians(a))
+        c = cos(radians(a))
+        self.__M = self.__M * matrix([[1, 0, 0, 0], [0, c, -s, 0], [0, s, c, 0], [0, 0, 0, 1]],dtype=float32)
+
+    def rotate_y(self, a):
+        s = sin(radians(a))
+        c = cos(radians(a))
+        self.__M = self.__M * matrix([[c, 0, s, 0], [0, 1, 0, 0], [-s, 0, c, 0], [0, 0, 0, 1]],dtype=float32)
+
+    def rotate_z(self, a):
+        s = sin(radians(a))
+        c = cos(radians(a))
+        self.__M = self.__M * matrix([[c, -s, 0, 0], [s, c, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],dtype=float32);
+
+    def perspective(self,fov, aspect, n, f):
+        h = tan(radians(fov) * .5)
+        w = h * aspect
+        dz = f - n
+        self.__M = matrix([[1./w,0,0,0],[0,1./h,0,0],[0,0,-(f+n)/dz,-2. * f * n / dz],[0,0,-1,0]],dtype = float32)
--- shaders.py
+++ shaders.py
@@ -3,92 +3,100 @@ from miniengine import *
 def simple() :
     return Material(
         '''
-            attribute vec3  pos;
-            attribute vec2  tex;
-            uniform float time;
-            varying   vec2  v_tex;
+            attribute vec3 pos;
+            attribute vec2 tex;
+            uniform   mat4 modelView;
+            uniform   mat4 prjView;
+            varying   vec3 v_pos;
+            varying   vec2 v_tex;
+
             void main()
             {
                 v_tex=tex;
-                gl_Position = vec4(pos * cos(time),1);
+                v_pos=(pos + 1.)/2.0;
+                gl_Position = vec4(pos,1) * modelView * prjView;
             }
         ''',
         '''
             uniform float time;
             varying vec2  v_tex;
+            varying vec3 v_pos;
+
             void main()
             {
-                gl_FragColor = vec4(v_tex,sin(time*10.0),1);
+                gl_FragColor = vec4(v_pos,1);
             }
         '''
     )
 
 
-def zebro() :
+def cells() :
     return Material(
         '''
             attribute vec3  pos;
             attribute vec2  tex;
+            uniform mat4 modelView;
+            uniform mat4 prjView;
+
             varying   vec2  v_tex;
             void main()
             {
                 v_tex=tex;
-                gl_Position = vec4(pos,1);
+                gl_Position = vec4(pos,1) * modelView * prjView;
             }
         ''',
         '''
             uniform float time;
             varying vec2  v_tex;
+
             void main()
             {
-                float x = 0.5 - v_tex.x;
-                float y = 0.5 - v_tex.y;
-                float r = (x * x + y * y);
-                float z = cos((r +  time * 0.2)/0.01);
-                gl_FragColor = vec4(z,z,z,1);
-           }
+                float c = .1/cos((v_tex.x*32.+(time/50.)*100.));
+                c += .1/cos((v_tex.y*32.+(time/50.)*100.));
+                c += sin(v_tex.x*30.);
+
+	            gl_FragColor = vec4(0,c,0,1.0 );
+            }
+
         '''
     )
 
-
-def flag():
+def water() :
     return Material(
         '''
             attribute vec3  pos;
             attribute vec2  tex;
-            uniform float time;
+            uniform mat4 modelView;
+            uniform mat4 prjView;
+
             varying   vec2  v_tex;
-            varying  vec3 v_pos;
             void main()
             {
                 v_tex=tex;
-                v_pos = pos;
-                if ((tex.x>0.3)&&(tex.x<0.8))
-                    {
-                    gl_Position = vec4(pos.x, pos.y-(sin(time*10.0)/10.0),pos.z,1);
-                    }
-                else
-                    {
-                    gl_Position = vec4(pos,1);
-                    }
+                gl_Position = vec4(pos,1) * modelView * prjView;
             }
         ''',
         '''
             uniform float time;
             varying vec2  v_tex;
 
-            varying  vec3 v_pos;
+            #define MAX_ITER 8
+
             void main()
             {
-                if (sqrt((v_tex.x - 0.3)*(v_tex.x - 0.3) + (v_tex.y - 0.5)*(v_tex.y - 0.5)) < 0.2)
-                    {
-                    gl_FragColor = vec4(1,0,0,0);
-                    }
-                else
-                    {
-                    gl_FragColor = vec4((v_pos+1.0)/2.0,1);
-
-                    }
-            }
+	            vec2 p = v_tex * 8.0- vec2(30.0);
+	            vec2 i = p;
+	            float c = 1.0;
+	            float inten = .05;
+	            for (int n = 0; n < MAX_ITER; n++)
+	            {
+		            float t = time * (1.0 - (3.0 / float(n+1)));
+		            i = p + vec2(cos(t - i.x) + sin(t + i.y), sin(t - i.y) + cos(t + i.x));
+		            c += 1.0/length(vec2(p.x / (sin(i.x+t)/inten),p.y / (cos(i.y+t)/inten)));
+	            }
+	            c /= float(MAX_ITER);
+	            c = 1.5-sqrt(c);
+	            gl_FragColor = vec4(pow(c, 7.0)) + vec4(0.0, 0.15, 0.25, 1.0);
+}
         '''
     )
