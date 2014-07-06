eb011fd25ee037bc0a3ed606d117f91168ff3e11
Ururu <besteto@gmail.com>
2014-07-06 15:16:16 +0400

    ToDo: add mouse

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ m5 = Material('BUMP_LOL', './data/shaders/base.vsh', './data/shaders/bump_lol.fs
 
 back = geometry.plane(64, 64).set_material(m5).set_texture([t2,t6]).translate(0, 0, -80)
 cub = geometry.cube(15,15,15).set_material(m5).set_texture([t2,t6]).translate(0,0,-50)
-#sph1 = geometry.sphere(4, 10).set_material(m5).set_texture([eye,t6]).translate(5, 3, -30).rotate_x(180).rotate_y(5)
-#sph2 = geometry.sphere(4, 64).set_material(m1).set_texture([eye]).translate(-5, 3, -30).rotate_x(180).rotate_y(-5)
+sph1 = geometry.sphere(4, 10).set_material(m5).set_texture([eye,t6]).translate(5, 3, -30).rotate_x(180).rotate_y(5)
+sph2 = geometry.sphere(4, 64).set_material(m1).set_texture([eye]).translate(-5, 3, -30).rotate_x(180).rotate_y(-5)
 
 def update(dt):
 #    pass
