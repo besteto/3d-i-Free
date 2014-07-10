799323160a7a6d00c8fa685120119909320a6a48
Ururu <besteto@gmail.com>
2014-07-10 21:14:42 +0400

    look, im in cosmos now

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ t3 = Texture('./data/ifree.jpg')
 t4 = Texture('./data/zw.jpg')
 t5 = Texture('./data/ScreenShot.jpg')
 t6 = Texture('./data/normalmap.jpg')
+t7 = Texture('./data/Ancient_Stars.jpg')
 eye = Texture('./data/eye_texture_3_flattened-JPEG-BIG.jpg')
 
 m1 = Material('SIMPLE', './data/shaders/base.vsh', './data/shaders/simple.fsh')
 m2 = Material('BLUR', './data/shaders/blur_base.vsh', './data/shaders/blur_simple.fsh')
 
-back = geometry.plane(1, 1).set_material(m2).set_texture([t3]).translate(0, 0, -1)
+#back = geometry.plane(1, 1).set_material(m2).set_texture([t3]).translate(0, 0, -1)
+sph = geometry.sphere(50,32).set_material(m1).set_texture([t7])
 
 def update(dt):
     pass
