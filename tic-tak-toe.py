69083d25148c8755f1c9daf337a9e253f70f4565
Ururu <besteto@gmail.com>
2014-08-12 22:25:53 +0400

    tic-tak-toe, bonezz

--- tic-tak-toe.py
+++ tic-tak-toe.py
@@ -1,5 +1,7 @@
 from miniGL import *
 
+e = Engine(800, 600)
+
 cubemap = Texture("./data/cubemaps/XO.jpg")
 normalcubemap = Texture("./data/nmaps/cubenm.jpg")
 mBlinn = Material('BLINN', './data/shaders/base.vsh', './data/shaders/simple_blinn.fsh')
@@ -27,5 +29,42 @@ class tttBoard():
                 cube = tttCube(x-2, y-2, -4)
                 self.cubes[(x, y)] = cube
 
-    def marker(self):
-        pass
+    def marked(self, cubNumb):
+        self.cubes(cubNumb).mark
+
+
+def update(dt):
+    #pass
+    for everyC in cubes:
+        everyC.rotate_y(dt*50)
+
+def mmove(mx, my):
+    dx = mx - e.prevMouse[0]
+    dy = my - e.prevMouse[1]
+    e.prevMouse = [mx, my]
+    for every in cubes:
+        every.rotate_y(dx)
+        every.rotate_x(dy)
+
+def mclick(GLUT_RIGHT_BUTTON, GLUT_UP, mx, my):
+
+    if mx/800 < 0.3:
+        x = 1
+    elif mx/800 < 0.6:
+        x = 2
+    else:
+        x = 3
+
+    if my/600 < 0.3:
+        y = 1
+    elif my/600 < 0.6:
+        y = 2
+    else:
+        y = 3
+
+    tulp = (x, y)
+    tttBoard.marked(tulp)
+
+e.set_mousehandlers(mmove, mclick)
+e.set_update(update)
+e.loop()
